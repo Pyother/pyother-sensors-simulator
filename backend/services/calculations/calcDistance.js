563ad3a64e3e4b2fb5963fc6457853bbf269294d
pyother <piotr.dominik.sobol@gmail.com>
2025-03-07 23:35:08 +0100

    Function for object boundaries calculation

--- backend/services/calculations/calcDistance.js
+++ backend/services/calculations/calcDistance.js
@@ -1,7 +1,24 @@
+const calcObjectBoundaries = require('./calcObjectBoundaries');
+
 const calcDistance = ({ position, direction, sensor, inputObject }) => {
 
+    const sensorPath = {
+        start: { x: position.x, y: position.y }
+        line: { 
+            a: Math.tan(direction),
+            b: position.y - Math.tan(direction) * position.x,
+            horizontal: direction === 0 || direction === 180 || direction === -180 ?
+                { y: position.y } : false,
+            vertical: direction === 90 || direction === -90 ?
+                { x: position.x } : false
+        }
+    };
+
+    const objectBoundaries = calcObjectBoundaries({ inputObject });
+    const distances = [];
+
     
-    
+
 }
 
 module.exports = calcDistance;
--- backend/services/calculations/calcObjectBoundaries.js
+++ backend/services/calculations/calcObjectBoundaries.js
@@ -5,22 +5,28 @@ const calcObjectBoundaries = ({ inputObject }) => {
     inputObject.forEach((object, index) => {
         if(index !== inputObject.length - 1) {
 
-            const a = inputObject[index + 1].x === object.x ? 0 :
-                (inputObject[index + 1].y - object.y) / (inputObject[index + 1].x - object.x);
-            const b = inputObject[index + 1].y === object.y ? 0 : object.y - a * object.x;
+            const nextObject = inputObject[index + 1];
+            let [vertical, horizontal] = [false, false];
+            let [a, b] = [0, 0];
+
+            nextObject.x === object.x ?  
+                vertical = { x: object.x } : 
+                a = (nextObject.y - object.y) / (nextObject.x - object.x);
+    
+            nextObject.y === object.y ? 
+                horizontal = { y: object.y } : 
+                b = object.y - a * object.x;
 
             boundaries.push({ 
                 start: { x: object.x, y: object.y },
-                end: { x: inputObject[index + 1].x, y: inputObject[index + 1].y },
-                line: { a, b }
+                end: { x: nextObject.x, y: nextObject.y },
+                line: { a, b, horizontal, vertical }
             });
         }
     })
 
     return boundaries;
 }
-
-console.log(calcObjectBoundaries({ inputObject: [{ x: 0, y: 0 }, { x: 2, y: 1 }, { x: 2, y: 2 }] }));
 
 module.exports = calcObjectBoundaries;
 
