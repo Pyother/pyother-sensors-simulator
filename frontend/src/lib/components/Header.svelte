85ebf8e93842a19c886df36ae4c3eb957e356e31
Pyother <piotr.dominik.sobol@gmail.com>
2025-05-21 15:07:40 +0200

    Recreate frontend in Svelte

---
+++ frontend/src/lib/components/Header.svelte
@@ -0,0 +1,29 @@
+<script>
+    import logo from '$lib/assets/logo_sensors.webp';
+    import { goto } from '$app/navigation';
+</script>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm p-3 px-lg-4 py-lg-3">
+    <div class="container-fluid p-0">
+        <div class="d-flex align-items-center">
+            <a href="https://about.pyother.pl">
+                <img src={logo} alt="Logo" style="width: 48px; height: 48px;" />
+            </a>
+            <span class="navbar-brand mb-0 ms-3 fs-5 h1 d-none d-lg-inline">pyother-sensors-simulator</span>
+        </div>
+        <div class="ms-auto d-flex gap-3">
+            <button 
+                class="btn btn-outline-secondary"
+                on:click={() => goto('/')}
+            >
+                Home
+            </button>
+            <button 
+                class="btn btn-outline-secondary"
+                on:click={() => goto('/stats')}
+            >
+                Stats
+            </button>
+        </div>
+    </div>
+</nav>
---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    // Imports:
+    import "../app.css";
+    import Header from '$lib/components/Header.svelte';
+
+</script>
+
+<Header />
+<div class="px-4 py-4 px-lg-custom">
+    <slot/>
+</div>
